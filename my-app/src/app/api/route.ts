d3bcddb86bf7c0bcecb404573b8d8b79a3a2c860
KarenLaines <144739875+KarenLaines@users.noreply.github.com>
2025-08-20 00:20:33 -0600

    feat(posts): endpoint para ver todos los posts

--- my-app/src/app/api/route.ts
+++ my-app/src/app/api/route.ts
@@ -1,62 +1,14 @@
-import { NextRequest, NextResponse } from "next/server";
-import pool from "@/libs/db" //  conexión a PostgreSQL
+// app/api/posts/route.ts
+import { NextResponse } from "next/server";
+import { PostRepository } from "@repositories/posts_repository";
 
+const repo = new PostRepository();
 
-export async function POST(request: NextRequest) {
-  const data = await request.json();
-
+export async function GET() {
   try {
-    // Validar datos
-    isValid(data);
-
-    // Insertar en la base de datos
-    const query = `
-      INSERT INTO posts (title, description, author)
-      VALUES ($1, $2, $3)
-      RETURNING *;
-    `;
-    const values = [data.title, data.description, data.author];
-    const result = await pool.query(query, values);
-
-    return NextResponse.json(
-      { message: "Post created 🚀", post: result.rows[0] },
-      { status: 201 }
-    );
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      return NextResponse.json({ error: error.message }, { status: 400 });
-    }
-    return NextResponse.json({ error: "Unexpected error" }, { status: 500 });
-  }
-}
-
-
-
-
-
-// --------------------------
-// VALIDATIONS
-// --------------------------
-function isValid(post: { title: string; description: string; author: string }) {
-  validateTitle(post.title);
-  validateDescription(post.description);
-  validateAuthor(post.author);
-}
-
-function validateTitle(title: string) {
-  if (typeof title !== "string" || title.trim().length === 0 || title.length > 40) {
-    throw new Error("Title is required and must be at most 40 characters.");
-  }
-}
-
-function validateDescription(description: string) {
-  if (typeof description !== "string" || description.trim().length < 10) {
-    throw new Error("Description is required and must be at least 10 characters.");
-  }
-}
-
-function validateAuthor(author: string) {
-  if (typeof author !== "string" || author.trim().length === 0 || author.length > 30) {
-    throw new Error("Author is required and must be at most 30 characters.");
+    const posts = await repo.getAll();
+    return NextResponse.json(posts);
+  } catch (err) {
+    return NextResponse.json({ error: "Error to get posts" }, { status: 500 });
   }
 }
