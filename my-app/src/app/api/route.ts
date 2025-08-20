90bb4375c16020a9c42b8b2d0a4671b38d3ebc06
KarenLaines <144739875+KarenLaines@users.noreply.github.com>
2025-08-20 00:21:07 -0600

    feat(posts): editar un post

--- my-app/src/app/api/route.ts
+++ my-app/src/app/api/route.ts
@@ -1,14 +1,15 @@
-// app/api/posts/route.ts
-import { NextResponse } from "next/server";
-import { PostRepository } from "@repositories/posts_repository";
+// app/api/posts/[id]/route.ts
+import { NextRequest, NextResponse } from "next/server";
+import { PostRepository } from "@repositories/PostRepository";
 
 const repo = new PostRepository();
 
-export async function GET() {
+export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
+  const data = await request.json();
   try {
-    const posts = await repo.getAll();
-    return NextResponse.json(posts);
+    const updated = await repo.update(Number(params.id), data);
+    return NextResponse.json(updated);
   } catch (err) {
-    return NextResponse.json({ error: "Error to get posts" }, { status: 500 });
+    return NextResponse.json({ error: "Error al actualizar post" }, { status: 500 });
   }
 }
